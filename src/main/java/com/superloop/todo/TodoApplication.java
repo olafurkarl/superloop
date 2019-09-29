56babe7e66b4d0620d018749f7a9b24e8400413d
Ólafur Karl Stefánsson <olafur@trackwell.com>
2019-09-30 09:44:13 +1000

    Setting server timezone to UTC.

--- src/main/java/com/superloop/todo/TodoApplication.java
+++ src/main/java/com/superloop/todo/TodoApplication.java
@@ -3,11 +3,20 @@ package com.superloop.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TodoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        // We want all dates to be stored and validated in UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
 }
